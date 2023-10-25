36caaaf1b19a4660218805128d5d2339d76208b0
DavideCavallotto <davidecavallotto92@gmail.com>
2023-10-25 13:56:43 +0200

    ADD html, js,css files and markup

---
+++ script.js
@@ -0,0 +1,17 @@
+const createApp = Vue.createApp
+
+createApp ({
+    data () {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+        console.log('ciao')
+    }
+
+
+}).mount('#app')
