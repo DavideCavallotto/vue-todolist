724941123785cfb5dcd5346469f11a48b0604234
DavideCavallotto <davidecavallotto92@gmail.com>
2023-10-25 15:04:29 +0200

    ADD array task and cross for remove task

--- script.js
+++ script.js
@@ -3,11 +3,28 @@ const createApp = Vue.createApp
 createApp ({
     data () {
         return {
+            
+            todos: [
+                {
+                    text: 'Fare i compiti',
+                    done: false
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Fare il bucato',
+                    done: false
+                }
+            ]
 
         }
     },
     methods: {
-
+        removeTask(index) {
+            this.todos.splice(index , 1)
+        }
     },
     mounted() {
         console.log('ciao')
